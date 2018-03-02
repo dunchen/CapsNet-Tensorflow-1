d0aba52512d2d0e9669dbf78ddf3ab816abaf34f
dunchen <cdun@macalester.edu>
2018-03-01 21:01:45 -0600

    first try

--- caps-ica.py
+++ caps-ica.py
@@ -1,10 +1,11 @@
-from __future__ import division, print_function, absolute_import
+
 import tensorflow as tf
 
 from config import cfg
 from utils import get_batch_data
 from utils import softmax
 from utils import reduce_sum
+
 import numpy as np
 # Import MNIST data
 from tensorflow.examples.tutorials.mnist import input_data
@@ -304,7 +305,7 @@ class CapsLayer(object):
         if self.layer_type == 'FC':
             if self.with_routing:
                 # the DigitCaps layer, a fully connected layer
-                # Reshape the input into [batch_size, 1152, 1, 8, 1]
+                # Reshape the input into [batch_size, lc_out_h*lc_out_w, 1, 8, 1]
                 self.input = tf.reshape(input, shape=(batch_size, -1, 1, lc_out_channel, 1))
 
                 with tf.variable_scope('routing'):
@@ -318,16 +319,18 @@ class CapsLayer(object):
 
 class CapsNet(object):
     def __init__(self):
-        self.graph = tf.Graph()
-        with self.graph.as_default():
+        # self.graph = tf.Graph()
+        # with self.graph.as_default():
 
-            self.X = X
-            self.labels = Y
-            self.Y = Y
-            self.build_arch()
-            self.global_step = tf.Variable(0, name='global_step', trainable=False)
-            self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-            self.train_op = self.optimizer.minimize(self.total_loss, global_step=self.global_step)
+        self.X = X
+        self.labels = Y
+        self.Y = Y
+        self.build_arch()
+        self.loss()
+        self._summary()
+        self.global_step = tf.Variable(0, name='global_step', trainable=False)
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+        self.train_op = self.optimizer.minimize(self.total_loss, global_step=self.global_step)
 
 
         tf.logging.info('Seting up the main structure')
@@ -347,7 +350,7 @@ class CapsNet(object):
         lc1 = tf.nn.relu(lc1)   #[Batch, new_rows, new_cols,Channels)
 
         digitCaps = CapsLayer(num_outputs=10, vec_len=16, with_routing=True, layer_type='FC')
-        caps2 = digitCaps(lc1)
+        self.caps2 = digitCaps(lc1)
 
         # Decoder structure in Fig. 2
         # 1. Do masking, how:
@@ -357,12 +360,12 @@ class CapsNet(object):
             self.v_length = tf.sqrt(reduce_sum(tf.square(self.caps2),
                                                axis=2, keepdims=True) + epsilon)
             self.softmax_v = softmax(self.v_length, axis=1)
-            assert self.softmax_v.get_shape() == [cfg.batch_size, 10, 1, 1]
+            #assert self.softmax_v.get_shape() == [cfg.batch_size, 10, 1, 1]
 
             # b). pick out the index of max softmax val of the 10 caps
             # [batch_size, 10, 1, 1] => [batch_size] (index)
             self.argmax_idx = tf.to_int32(tf.argmax(self.softmax_v, axis=1))
-            assert self.argmax_idx.get_shape() == [cfg.batch_size, 1, 1]
+            #assert self.argmax_idx.get_shape() == [cfg.batch_size, 1, 1]
             self.argmax_idx = tf.reshape(self.argmax_idx, shape=(cfg.batch_size, ))
 
             # Method 1.
@@ -376,7 +379,7 @@ class CapsNet(object):
                     masked_v.append(tf.reshape(v, shape=(1, 1, 16, 1)))
 
                 self.masked_v = tf.concat(masked_v, axis=0)
-                assert self.masked_v.get_shape() == [cfg.batch_size, 1, 16, 1]
+                #assert self.masked_v.get_shape() == [cfg.batch_size, 1, 16, 1]
             # Method 2. masking with true label, default mode
             else:
                 # self.masked_v = tf.matmul(tf.squeeze(self.caps2), tf.reshape(self.Y, (-1, 10, 1)), transpose_a=True)
@@ -388,9 +391,9 @@ class CapsNet(object):
         with tf.variable_scope('Decoder'):
             vector_j = tf.reshape(self.masked_v, shape=(cfg.batch_size, -1))
             fc1 = tf.contrib.layers.fully_connected(vector_j, num_outputs=512)
-            assert fc1.get_shape() == [cfg.batch_size, 512]
+            #assert fc1.get_shape() == [cfg.batch_size, 512]
             fc2 = tf.contrib.layers.fully_connected(fc1, num_outputs=1024)
-            assert fc2.get_shape() == [cfg.batch_size, 1024]
+            #assert fc2.get_shape() == [cfg.batch_size, 1024]
             self.decoded = tf.contrib.layers.fully_connected(fc2, num_outputs=784, activation_fn=tf.sigmoid)
 
     def loss(self):
@@ -401,7 +404,7 @@ class CapsNet(object):
         max_l = tf.square(tf.maximum(0., cfg.m_plus - self.v_length))
         # max_r = max(0, ||v_c||-m_minus)^2
         max_r = tf.square(tf.maximum(0., self.v_length - cfg.m_minus))
-        assert max_l.get_shape() == [batch_size, 10, 1, 1]
+        #assert max_l.get_shape() == [batch_size, 10, 1, 1]
 
         # reshape: [batch_size, 10, 1, 1] => [batch_size, 10]
         max_l = tf.reshape(max_l, shape=(batch_size, -1))
@@ -429,17 +432,103 @@ class CapsNet(object):
 
     # Summary
     def _summary(self):
-        train_summary = []
-        train_summary.append(tf.summary.scalar('train/margin_loss', self.margin_loss))
-        train_summary.append(tf.summary.scalar('train/reconstruction_loss', self.reconstruction_err))
-        train_summary.append(tf.summary.scalar('train/total_loss', self.total_loss))
-        recon_img = tf.reshape(self.decoded, shape=(cfg.batch_size, 28, 28, 1))
-        train_summary.append(tf.summary.image('reconstruction_img', recon_img))
-        self.train_summary = tf.summary.merge(train_summary)
+        # train_summary = []
+        # train_summary.append(tf.summary.scalar('train/margin_loss', self.margin_loss))
+        # train_summary.append(tf.summary.scalar('train/reconstruction_loss', self.reconstruction_err))
+        # train_summary.append(tf.summary.scalar('train/total_loss', self.total_loss))
+        # recon_img = tf.reshape(self.decoded, shape=(cfg.batch_size, 28, 28, 1))
+        # train_summary.append(tf.summary.image('reconstruction_img', recon_img))
+        # self.train_summary = tf.summary.merge(train_summary)
 
-        correct_prediction = tf.equal(tf.to_int32(self.labels), self.argmax_idx)
+        correct_prediction = tf.equal(tf.to_int32(tf.argmax(self.Y, 1)), self.argmax_idx)
         self.accuracy = tf.reduce_sum(tf.cast(correct_prediction, tf.float32))
 
+def routing(input, b_IJ):
+    ''' The routing algorithm.
+
+    Args:
+        input: A Tensor with [batch_size, num_caps_l=lc_out_h*lc_out_w, 1, length(u_i)=8, 1]
+               shape, num_caps_l meaning the number of capsule in the layer l.
+    Returns:
+        A Tensor of shape [batch_size, num_caps_l_plus_1, length(v_j)=16, 1]
+        representing the vector output `v_j` in the layer l+1
+    Notes:
+        u_i represents the vector output of capsule i in the layer l, and
+        v_j the vector output of capsule j in the layer l+1.
+     '''
+
+    # W: [1, num_caps_i, num_caps_j * len_v_j, len_u_j, 1]
+    W = tf.get_variable('Weight', shape=(1, lc_out_h*lc_out_w, 160, lc_out_channel, 1), dtype=tf.float32,
+                        initializer=tf.random_normal_initializer(stddev=cfg.stddev))
+    biases = tf.get_variable('bias', shape=(1, 1, 10, 16, 1))
+
+    # Eq.2, calc u_hat
+    # Since tf.matmul is a time-consuming op,
+    # A better solution is using element-wise multiply, reduce_sum and reshape
+    # ops instead. Matmul [a, b] x [b, c] is equal to a series ops as
+    # element-wise multiply [a*c, b] * [a*c, b], reduce_sum at axis=1 and
+    # reshape to [a, c]
+    input = tf.tile(input, [1, 1, 160, 1, 1])
+    #assert input.get_shape() == [cfg.batch_size, lc_out_h*lc_out_w, 160, 8, 1]
+
+    u_hat = tf.reduce_sum(W * input, axis=3, keepdims=True)
+    u_hat = tf.reshape(u_hat, shape=[-1, lc_out_h*lc_out_w, 10, 16, 1])
+    #assert u_hat.get_shape() == [cfg.batch_size, lc_out_h*lc_out_w, 10, 16, 1]
+
+    # In forward, u_hat_stopped = u_hat; in backward, no gradient passed back from u_hat_stopped to u_hat
+    u_hat_stopped = tf.stop_gradient(u_hat, name='stop_gradient')
+
+    # line 3,for r iterations do
+    for r_iter in range(cfg.iter_routing):
+        with tf.variable_scope('iter_' + str(r_iter)):
+            # line 4:
+            # => [batch_size, lc_out_h*lc_out_w, 10, 1, 1]
+            c_IJ = softmax(b_IJ, axis=2)
+
+            # At last iteration, use `u_hat` in order to receive gradients from the following graph
+            if r_iter == cfg.iter_routing - 1:
+                # line 5:
+                # weighting u_hat with c_IJ, element-wise in the last two dims
+                # => [batch_size, lc_out_h*lc_out_w, 10, 16, 1]
+                s_J = tf.multiply(c_IJ, u_hat)
+                # then sum in the second dim, resulting in [batch_size, 1, 10, 16, 1]
+                s_J = reduce_sum(s_J, axis=1, keepdims=True) + biases
+                #assert s_J.get_shape() == [cfg.batch_size, 1, 10, 16, 1]
+
+                # line 6:
+                # squash using Eq.1,
+                v_J = squash(s_J)
+                #assert v_J.get_shape() == [cfg.batch_size, 1, 10, 16, 1]
+            elif r_iter < cfg.iter_routing - 1:  # Inner iterations, do not apply backpropagation
+                s_J = tf.multiply(c_IJ, u_hat_stopped)
+                s_J = reduce_sum(s_J, axis=1, keepdims=True) + biases
+                v_J = squash(s_J)
+
+                # line 7:
+                # reshape & tile v_j from [batch_size ,1, 10, 16, 1] to [batch_size, lc_out_h*lc_out_w, 10, 16, 1]
+                # then matmul in the last tow dim: [16, 1].T x [16, 1] => [1, 1], reduce mean in the
+                # batch_size dim, resulting in [1, lc_out_h*lc_out_w, 10, 1, 1]
+                v_J_tiled = tf.tile(v_J, [1, lc_out_h*lc_out_w, 1, 1, 1])
+                u_produce_v = reduce_sum(u_hat_stopped * v_J_tiled, axis=3, keepdims=True)
+                #assert u_produce_v.get_shape() == [cfg.batch_size, lc_out_h*lc_out_w, 10, 1, 1]
+
+                # b_IJ += tf.reduce_sum(u_produce_v, axis=0, keep_dims=True)
+                b_IJ += u_produce_v
+
+    return(v_J)
+
+
+def squash(vector):
+    '''Squashing function corresponding to Eq. 1
+    Args:
+        vector: A tensor with shape [batch_size, 1, num_caps, vec_len, 1] or [batch_size, num_caps, vec_len, 1].
+    Returns:
+        A tensor with the same shape as vector but squashed in 'vec_len' dimension.
+    '''
+    vec_squared_norm = reduce_sum(tf.square(vector), -2, keepdims=True)
+    scalar_factor = vec_squared_norm / (1 + vec_squared_norm) / tf.sqrt(vec_squared_norm + epsilon)
+    vec_squashed = scalar_factor * vector  # element-wise
+    return(vec_squashed)
 
 model=CapsNet()
 
@@ -455,11 +544,12 @@ with tf.Session() as sess:
     tf.stop_gradient(pretrained_weights)
 
     for step in range(1, num_steps+1):
+        print(step)
         batch_x, batch_y = mnist.train.next_batch(batch_size)
         # Run optimization op (backprop)
-        _, loss, train_acc, summary_str = sess.run([model.train_op, model.total_loss, model.accuracy],
+        _, loss, train_acc = sess.run([model.train_op, model.total_loss, model.accuracy],
                 feed_dict={X: batch_x, Y: batch_y})
-        sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
+        sess.run([model.train_op], feed_dict={X: batch_x, Y: batch_y})
         if step % display_step == 0 or step == 1:
             # Calculate batch loss and accuracy
             loss, acc = sess.run([model.total_loss, model.accuracy], feed_dict={X: batch_x,
